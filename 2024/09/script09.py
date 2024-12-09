8eff0e299d3d67fa1154e3001ee5fa1fe4d2b706
Paolo Faustini <paolo.faustini98@gmail.com>
2024-12-09 22:11:33 +0100

    Day 9 part 2 Python (still some optimization to be done)

--- 2024/09/script09.py
+++ 2024/09/script09.py
@@ -1,12 +1,10 @@
 FILE_NAME = "example"
-# FILE_NAME = "input"
+FILE_NAME = "input"
 
 with open(FILE_NAME) as file:
     data = [int(v) for v in file.readline().strip()]
 
 filesystem = []
-
-# count =
 
 for i,length in enumerate(data):
     # even index: file
@@ -24,8 +22,7 @@ i = 0
 for j,v in enumerate(reversed(filesystem)):
     if v < 0:
         continue
-
-
+    
     while fs1[i] >= 0:
         i += 1
 
@@ -34,7 +31,6 @@ for j,v in enumerate(reversed(filesystem)):
 
     fs1[i] = v
     fs1[-j-1] = -1
-
 
 tot = 0
 for i,v in enumerate(fs1):
@@ -46,3 +42,37 @@ for i,v in enumerate(fs1):
 print("Part One:")
 print(tot)
 
+
+
+spaces = []
+files = []
+p = 0
+for i,length in enumerate(data):
+    # even index: file
+    if i % 2 == 0:
+        files.append((i // 2, p, length))
+    else:
+        spaces.append((p, length))
+    
+    p += length
+
+tot = 0
+for i,pos,length in reversed(files):
+    for s,(s_pos,s_length) in enumerate(spaces):
+        if s_pos >= pos:
+            tot += i * length * (2 * pos + length - 1) // 2
+            break
+        
+        if s_length >= length:
+            tot += i * length * (2 * s_pos + length - 1) // 2
+            
+            if s_length > length:
+                spaces[s] = (s_pos + length, s_length - length)
+            else:
+                spaces.pop(s)
+            
+            break
+
+print()
+print("Part Two:")
+print(tot)
